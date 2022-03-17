aa064c97e389e22833630ee7cb98f348d2e5c47a
Enzo Mourany <enzo.mourany@outlook.com>
2022-03-17 09:12:46 +0100

    added topbar and main code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MainPage from './main.jsx';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App" >
+      <MainPage />
+    </div>
+  );
+
+}
+
+export default App;
