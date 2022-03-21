b9d51da3b81fb3650df332fad4326c2af5fff144
Enzo Mourany <enzo.mourany@outlook.com>
2022-03-21 08:30:21 +0100

    menu --> added ton App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './app.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Technologies from "./components/technologies/Technologies";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="App" >
       <Topbar />
