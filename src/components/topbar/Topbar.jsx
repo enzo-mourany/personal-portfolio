8e57f926034aca1c4c7ca503154b252e5cd15334
Enzo Mourany <enzo.mourany@outlook.com>
2022-03-21 08:43:01 +0100

    transform burgerMenu animation

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,7 @@ import { LinkedIn, Mail, GitHub } from "@material-ui/icons";
 
 export default function topbar() {
     return (
-        <div className="topbar">
+        <div className="topbar active">
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">main</a>
