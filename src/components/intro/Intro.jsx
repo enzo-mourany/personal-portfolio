8d3a905258d6573d1a80c6e7136103b3aa2f4375
Enzo Mourany <enzo.mourany@outlook.com>
2022-03-21 11:53:13 +0100

    added intro elements

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,33 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+
+
+            <div className="left">
+
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Enzo Mourany</h1>
+                    <h3>
+                        Frontend Developer
+                    </h3>
+                </div>
+                <div className="buttons">
+                    <div className="learnMore">
+                        <a href="#portfolio">
+                            Learn More
+                        </a>
+                    </div>
+                    <div className="downLoadPdf">
+                        <a href="#">
+                            Download CV
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
