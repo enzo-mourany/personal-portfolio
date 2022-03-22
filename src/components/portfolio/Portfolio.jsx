f39f3b013f8751f4eead2f0a5a8ddc334037d217
Enzo Mourany <enzo.mourany@outlook.com>
2022-03-22 19:04:30 +0100

    added project component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <div className="wrapper">
+                <div className="title">
 
+                </div>
+                <div className="container">
+                    <div className="item">
+                        <img
+                            src=""
+                            alt=""
+                        />
+                        <h3>Tabata App</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
