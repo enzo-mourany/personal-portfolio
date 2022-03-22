03240f501a92e17881f8e2d23818e476d4861fec
Enzo Mourany <enzo.mourany@outlook.com>
2022-03-22 22:31:26 +0100

    use data featuredPortfolio for Portfolio.js

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,11 @@
 import "./portfolio.scss"
+import { useState, useEffect } from 'react';
+import featuredPortfolio from "../../data";
 
 export default function Portfolio() {
+
+    const data = featuredPortfolio;
+
     return (
         <div className="portfolio" id="portfolio">
             <div className="wrapper">
@@ -8,48 +13,15 @@ export default function Portfolio() {
                     <h2>Portfolio</h2>
                 </div>
                 <div className="container">
-                    <div className="item">
-                        <img
-                            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fhext.to%2Fweb-development%2F&psig=AOvVaw1pNHPIbVBFXnBdcNTatYWr&ust=1648068152150000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOit3bnK2vYCFQAAAAAdAAAAABAD"
-                            alt=""
-                        />
-                        <h3>Tabata App</h3>
-                    </div>
-                    <div className="item">
-                        <img
-                            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fhext.to%2Fweb-development%2F&psig=AOvVaw1pNHPIbVBFXnBdcNTatYWr&ust=1648068152150000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOit3bnK2vYCFQAAAAAdAAAAABAD"
-                            alt=""
-                        />
-                        <h3>Tabata App</h3>
-                    </div>
-                    <div className="item">
-                        <img
-                            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fhext.to%2Fweb-development%2F&psig=AOvVaw1pNHPIbVBFXnBdcNTatYWr&ust=1648068152150000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOit3bnK2vYCFQAAAAAdAAAAABAD"
-                            alt=""
-                        />
-                        <h3>Tabata App</h3>
-                    </div>
-                    <div className="item">
-                        <img
-                            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fhext.to%2Fweb-development%2F&psig=AOvVaw1pNHPIbVBFXnBdcNTatYWr&ust=1648068152150000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOit3bnK2vYCFQAAAAAdAAAAABAD"
-                            alt=""
-                        />
-                        <h3>Tabata App</h3>
-                    </div>
-                    <div className="item">
-                        <img
-                            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fhext.to%2Fweb-development%2F&psig=AOvVaw1pNHPIbVBFXnBdcNTatYWr&ust=1648068152150000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOit3bnK2vYCFQAAAAAdAAAAABAD"
-                            alt=""
-                        />
-                        <h3>Tabata App</h3>
-                    </div>
-                    <div className="item">
-                        <img
-                            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fhext.to%2Fweb-development%2F&psig=AOvVaw1pNHPIbVBFXnBdcNTatYWr&ust=1648068152150000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOit3bnK2vYCFQAAAAAdAAAAABAD"
-                            alt=""
-                        />
-                        <h3>Tabata App</h3>
-                    </div>
+                    {data.map((d) => (
+                        <div className="item">
+                            <img
+                                src={d.img}
+                                alt=""
+                            />
+                            <h3>{d.title}</h3>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
