47e7dc04bf4ff7210d999131ac2f4d1e608f0fb6
Enzo Mourany <enzo.mourany@outlook.com>
2022-03-27 14:24:49 +0200

    portfolio grid items added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -67,7 +67,7 @@ export default function Portfolio() {
                                             </svg>
                                         </div>
                                         <div className="project-links">
-                                            <a href={featuredPortfolio[0].githubLink} aria-label="GitHub Link" target="_blank" rel="noopener noreferrer">
+                                            <a href={featuredPortfolio[1].githubLink} aria-label="GitHub Link" target="_blank" rel="noopener noreferrer">
                                                 <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
                                                     <title>GitHub</title>
                                                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
@@ -84,44 +84,9 @@ export default function Portfolio() {
                                             </a>
                                         </div>
                                     </div>
-                                    <h3 className="project-title">test</h3>
-                                    <div className="project-description"></div>
-                                </div>
-                                <div className="bottom">
-                                    <ul className="project-tech-list"></ul>
-                                </div>
-                            </div>
-                        </li>
-                        <li className="item">
-                            <div className="project-inner">
-                                <div className="top">
-                                    <div className="project-top">
-                                        <div className="folder">
-                                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-folder">
-                                                <title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-                                                <title>Folder</title>
-                                                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-                                            </svg>
-                                        </div>
-                                        <div className="project-links">
-                                            <a href={featuredPortfolio[0].githubLink} aria-label="GitHub Link" target="_blank" rel="noopener noreferrer">
-                                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
-                                                    <title>GitHub</title>
-                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
-                                                    </path>
-                                                </svg>
-                                            </a>
-                                            <a href="https://medium.com/stories-from-upstatement/integrating-algolia-search-with-wordpress-multisite-e2dea3ed449c" aria-label="External Link" class="external" target="_blank" rel="noopener noreferrer">
-                                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
-                                                    <title>External Link</title>
-                                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                                    <polyline points="15 3 21 3 21 9"></polyline>
-                                                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                                                </svg>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <h3 className="project-title">test</h3>
+                                    <h3 className="project-title">
+                                        <a href="#">{featuredPortfolio[1].title}</a>
+                                    </h3>
                                     <div className="project-description"></div>
                                 </div>
                                 <div className="bottom">
@@ -140,7 +105,45 @@ export default function Portfolio() {
                                             </svg>
                                         </div>
                                         <div className="project-links">
-                                            <a href={featuredPortfolio[0].githubLink} aria-label="GitHub Link" target="_blank" rel="noopener noreferrer">
+                                            <a href={featuredPortfolio[2].githubLink} aria-label="GitHub Link" target="_blank" rel="noopener noreferrer">
+                                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                    <title>GitHub</title>
+                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                                                    </path>
+                                                </svg>
+                                            </a>
+                                            <a href="https://medium.com/stories-from-upstatement/integrating-algolia-search-with-wordpress-multisite-e2dea3ed449c" aria-label="External Link" class="external" target="_blank" rel="noopener noreferrer">
+                                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
+                                                    <title>External Link</title>
+                                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                                    <polyline points="15 3 21 3 21 9"></polyline>
+                                                    <line x1="10" y1="14" x2="21" y2="3"></line>
+                                                </svg>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <h3 className="project-title">
+                                        <a href="#">{featuredPortfolio[2].title}</a>
+                                    </h3>
+                                    <div className="project-description"></div>
+                                </div>
+                                <div className="bottom">
+                                    <ul className="project-tech-list"></ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="project-inner">
+                                <div className="top">
+                                    <div className="project-top">
+                                        <div className="folder">
+                                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-folder">
+                                                <title>Folder</title>
+                                                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+                                            </svg>
+                                        </div>
+                                        <div className="project-links">
+                                            <a href={featuredPortfolio[3].githubLink} aria-label="GitHub Link" target="_blank" rel="noopener noreferrer">
                                                 <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
                                                     <title>GitHub</title>
                                                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
@@ -158,7 +161,9 @@ export default function Portfolio() {
                                             </a>
                                         </div>
                                     </div>
-                                    <h3 className="project-title">test</h3>
+                                    <h3 className="project-title">
+                                        <a href="#">{featuredPortfolio[3].title}</a>
+                                    </h3>
                                     <div className="project-description"></div>
                                 </div>
                                 <div className="bottom">
