63012bb4c9ec377604f82a10a12f2dead0d0420a
Enzo Mourany <enzo.mourany@outlook.com>
2022-04-08 04:57:23 +0200

    added contact title and responsive render

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,8 +4,13 @@ export default function Contact() {
     return (
         <div className="contact" id="contact">
             <div className="wrapper">
-                <header></header>
+                <span className="big-text">CONTACT</span>
                 <div className="container">
+                    <div className="text">
+                        <h2>Get In <span>Touch</span></h2>
+                        <p>
+                            let's discuss your great project idea so that it can become a reality</p>
+                    </div>
                     <form action="">
                         <div className="formWord">
                             <div className="item">
