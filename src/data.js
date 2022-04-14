7a10baa0a7daa748fda329fc6b0085f375b4160d
Enzo Mourany <enzo.mourany@outlook.com>
2022-04-14 22:56:28 +0200

    added data link

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const featuredPortfolio = [{
         description: "A simple Tabata application compatible on IOS and Android. My React Native project",
         tech: ["React Native"],
         retex: "../public/PDF/retex-the-tabata-app.pdf",
-        githubLink: "www.google.com",
+        githubLink: "https://github.com/enzo-mourany/the-tabata-app.git",
         date: "2022"
     },
     {
@@ -14,7 +14,7 @@ export const featuredPortfolio = [{
         img: "../public/PNG/tabata-app.png",
         description: "Project for the IUT. Board game with two opposing players and IA development",
         tech: ["Java"],
-        retex: "link-test",
+        retex: "../public/PDF/retex-lowatem-IA.pdf",
         githubLink: "https://github.com/enzo-mourany/ia_lowatem",
         date: "2022"
     },
@@ -24,7 +24,7 @@ export const featuredPortfolio = [{
         img: "../public/PNG/tabata-app.png",
         description: "First project for the IUT. Development of the classic pong game with objects, collision, balls and sprites management.",
         tech: ["Java", "Processing"],
-        retex: "link-test",
+        retex: "../public/PDF/retex-pong.pdf",
         githubLink: "https://github.com/enzo-mourany/pong-java-iut",
         date: "2021"
     },
@@ -34,7 +34,7 @@ export const featuredPortfolio = [{
         img: "../public/PNG/tabata-app.png",
         description: "Project for the IUT. Creation of an electric skateboard rental and sale business. Realization of the PESTEL analysis and its website. Team project",
         tech: ["HTML", "CSS"],
-        retex: "link-test",
+        retex: "../public/PDF/retex-clajem.pdf",
         githubLink: "github-link test",
         date: "2021"
     },
@@ -44,7 +44,7 @@ export const featuredPortfolio = [{
         img: "../public/PNG/tabata-app.png",
         description: "Project for the IUT. Creation of an electric skateboard rental and sale business. Realization of the PESTEL analysis and its website. Team project",
         tech: ["Shell", "HTML"],
-        retex: "link-test",
+        retex: "../public/PDF/retex-storyboard.pdf",
         githubLink: "https://github.com/enzo-mourany/storyboard.git",
         date: "2021"
     },
@@ -54,7 +54,7 @@ export const featuredPortfolio = [{
         img: "../public/PNG/tabata-app.png",
         description: "Project for the IUT. Creation and conception of database to manage wine orders. Team project",
         tech: ["SQL"],
-        retex: "link-test",
+        retex: "../public/PDF/retex-wine-orders.pdf",
         githubLink: "https://github.com/enzo-mourany/wine-commands.git",
         date: "2022"
     },
