97dbac469b6edb0d96461f12f6c96665f5f81a0d
Enzo Mourany <enzo.mourany@outlook.com>
2022-04-16 14:02:10 +0200

    added adnimation on loaded

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,60 +1,74 @@
 import "./intro.scss";
+import { Fade } from 'react-reveal';
 
 export default function Intro() {
-
     return (
         <div className="intro" id="intro">
-            <div className="icons">
-                <div className="itemContainer">
-                    <a href="https://www.linkedin.com/in/enzo-mourany-9b4a37228/" target="_blank" rel="noreferrer">
-                        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" width="20" height="20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-linkedin"><title>LinkedIn</title><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
-                    </a>
-                </div>
-                <div className="itemContainer">
-                    <a href="https://github.com/enzo-mourany/" target="_blank" rel="noreferrer">
-                        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" width="20" height="20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-                    </a>
-                </div>
-                <div className="itemContainer">
-                    <a href="mailto:enzo.mourany@outlook.com">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-at" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <circle cx="12" cy="12" r="4" />
-                            <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28" />
-                        </svg>
-                    </a>
-                </div>
-            </div>
-            <div className="explore">
-                <div className="text">
+            <Fade delay={3000}>
 
-                    <p>EXPLORE</p>
+                <div className="icons">
+                    <div className="itemContainer">
+                        <a href="https://www.linkedin.com/in/enzo-mourany-9b4a37228/" target="_blank" rel="noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" width="20" height="20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-linkedin"><title>LinkedIn</title><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+                        </a>
+                    </div>
+                    <div className="itemContainer">
+                        <a href="https://github.com/enzo-mourany/" target="_blank" rel="noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" width="20" height="20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                        </a>
+                    </div>
+                    <div className="itemContainer">
+                        <a href="mailto:enzo.mourany@outlook.com">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-at" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <circle cx="12" cy="12" r="4" />
+                                <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28" />
+                            </svg>
+                        </a>
+                    </div>
                 </div>
-            </div>
+            </Fade>
+            <Fade delay={3000}>
+
+                <div className="explore">
+                    <div className="text">
+
+                        <p>EXPLORE</p>
+                    </div>
+                </div>
+            </Fade>
             <div className="left">
             </div>
             <div className="right">
                 <div className="wrapper">
-                    <h2>Hi There, I'm</h2>
-                    <h1>
-                        <span>Enzo</span> Mourany
-                    </h1>
-                    <h3>French / Frontend Developer</h3>
+
+                    <Fade bottom distance="20px" delay={1200}>
+                        <h2>Hi There, I'm</h2>
+                    </Fade>
+                    <Fade bottom distance="20px" delay={1400}>
+                        <h1>Enzo Mourany</h1>
+                    </Fade>
+                    <Fade bottom distance="20px" delay={1600}>
+                        <h3>French Frontend Developer</h3>
+                    </Fade>
 
                 </div>
-                <div className="buttons">
-                    <div className="learnMore">
-                        <a className="learnMoreBtn" href="#about">
-                            Learn More
-                        </a>
+                <Fade bottom cascade distance="10px" delay={2000}>
+
+                    <div className="buttons">
+                        <div className="learnMore">
+                            <a className="learnMoreBtn" href="#about">
+                                Learn More
+                            </a>
+                        </div>
+                        <div className="downloadPdf" >
+                            <a href="%PUBLIC_URL%/CV.pdf" target="_blank" rel="noopener noreferrer" download>Download CV</a>
+                        </div>
                     </div>
-                    <div className="downloadPdf" >
-                        <a href="%PUBLIC_URL%/CV.pdf" target="_blank" rel="noopener noreferrer" download>Download CV</a>
-                    </div>
-                </div>
+                </Fade>
             </div>
 
-        </div>
+        </div >
 
     )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,38 +1,54 @@
 import "./topbar.scss";
 import { LinkedIn, Mail, GitHub } from "@material-ui/icons";
+import { Fade } from 'react-reveal';
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
         <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo">Enzo</a>
-                    <div className="itemContainer">
-                        <a href="https://www.linkedin.com/in/enzo-mourany-9b4a37228/" target="_blank" rel="noreferrer">
-                            <LinkedIn className="icon" />
-                        </a>
-                    </div>
-                    <div className="itemContainer">
-                        <a href="https://github.com/enzo-mourany/" target="_blank" rel="noreferrer">
-                            <GitHub className="icon" />
-                        </a>
-                    </div>
-                    <div className="itemContainer">
-                        <a href="mailto:enzo.mourany@outlook.com">
-                            <Mail className="icon" />
-                        </a>
-                    </div>
+                    <Fade top distance="20px" delay={200} duration={500}>
+                        <a href="#intro" className="logo">Enzo</a>
+
+                    </Fade>
+                    <Fade top distance="20px" delay={300} duration={500}>
+                        <div className="itemContainer">
+                            <a href="https://www.linkedin.com/in/enzo-mourany-9b4a37228/" target="_blank" rel="noreferrer">
+                                <LinkedIn className="icon" />
+                            </a>
+                        </div>
+
+                    </Fade>
+                    <Fade top distance="20px" delay={400} duration={500}>
+                        <div className="itemContainer">
+                            <a href="https://github.com/enzo-mourany/" target="_blank" rel="noreferrer">
+                                <GitHub className="icon" />
+                            </a>
+                        </div>
+
+                    </Fade>
+                    <Fade top distance="20px" delay={500} duration={500}>
+
+                        <div className="itemContainer">
+                            <a href="mailto:enzo.mourany@outlook.com">
+                                <Mail className="icon" />
+                            </a>
+                        </div>
+                    </Fade>
                 </div>
                 <div className="right">
-                    <div
-                        className="hamburger"
-                        onClick={() => setMenuOpen(!menuOpen)}
+                    <Fade top distance="20px" delay={600} duration={500}>
 
-                    >
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
+                        <div
+                            className="hamburger"
+                            onClick={() => setMenuOpen(!menuOpen)}
+
+                        >
+                            <span className="line1"></span>
+                            <span className="line2"></span>
+                            <span className="line3"></span>
+                        </div>
+                    </Fade>
                 </div>
             </div>
         </div>
