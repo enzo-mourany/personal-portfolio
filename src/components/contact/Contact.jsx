16ee957a6c4757c0e0efb3ff7dac2ebc0cc8d526
Enzo Mourany <enzo.mourany@outlook.com>
2022-04-24 19:06:14 +0200

    responsive intro mobile

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,44 +11,11 @@ export default function Contact() {
                         <p>
                             let's discuss your great project idea so that it can become a reality</p>
                     </div>
-                    <form action="">
-                        <div className="formWord">
-                            <div className="form-group">
-                                <div className="item">
-
-                                    <span>Enter Full Name</span>
-                                    <input type="text"
-                                        className="inputName"
-                                        name="fullName"
-                                        placeholder="Your name"
-                                        required />
-                                </div>
-                                <div className="item">
-                                    <span>Enter Email</span>
-                                    <input type="text"
-                                        className="inputEmail"
-                                        name="email"
-                                        placeholder="Your email address"
-                                        required />
-
-                                </div>
-                                <div className="item">
-
-                                    <span>Message</span>
-                                    <textarea type="text"
-                                        className="inputMessage"
-                                        name="message"
-                                        placeholder="Your comment"
-                                        required />
-                                </div>
-                                <div className="item">
-                                    <button type="submit">
-                                        SUBMIT NOW
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+                    <div className="mail">
+                        <a href="mailto:enzo.mourany@outlook.com">
+                            Contact Me
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
